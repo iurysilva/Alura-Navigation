875c9b4a7c714bc948235c7d118fbde5ed6dcd65
iurysilva <iuryglabson18@gmail.com>
2022-09-20 09:27:15 -0300

    Feat: Navigate from Produtor to Cesta

--- src/rotas/ProdutorRotas.js
+++ src/rotas/ProdutorRotas.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
 import Produtor from "../telas/Produtor";
 import Home from "../telas/Home";
+import Cesta from "../telas/Cesta";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,5 +10,6 @@ export default function ProdutorRotas({ComponentePrincipal=Home}){
     return <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name='HomeScreen' component={ComponentePrincipal}/>
         <Stack.Screen name='Produtor' component={Produtor}/>
+        <Stack.Screen name='Cesta' component={Cesta}/>
     </Stack.Navigator>
 }
